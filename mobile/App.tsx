79242d452247229bfb7549ed9ad7261dcac6b482
Gabriel Soares <gabrielsoares@simrede.com.br>
2023-05-19 20:35:29 -0300

    Aula 2 - mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -2,10 +2,30 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { Text, View } from 'react-native';
 
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto';
+
+import {
+  BaiJamjuree_700Bold,
+} from '@expo-google-fonts/bai-jamjuree';
+
 export default function App() {
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold,
+  });
+
+  if (!hasLoadedFonts) {
+    return null;
+  }
+
   return (
-    <View className="bg-gray-950 flex-1 items-center justify-center">
-      <Text className="text-zinc-50 font-bold text-5xl">Capsula do Tempo</Text>
+    <View className="bg-gray-900 flex-1 items-center justify-center">
+      <Text className="text-gray-50 font-bold text-5xl">Capsula do Tempo</Text>
       <StatusBar style="light" />
     </View>
   );
